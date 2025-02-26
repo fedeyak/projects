7082313f099db8b48b454d7fbae3b9551f314f01
fedeyak <fedeyak@gmail.com>
2025-02-26 13:03:40 +0300

    add project

---
+++ java_interview_bot/src/main/java/gptBoot/java_interview_bot/JavaInterviewBotApplication.java
@@ -0,0 +1,13 @@
+package gptBoot.java_interview_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInterviewBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaInterviewBotApplication.class, args);
+	}
+
+}
---
+++ java_interview_bot/src/test/java/gptBoot/java_interview_bot/JavaInterviewBotApplicationTests.java
@@ -0,0 +1,13 @@
+package gptBoot.java_interview_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInterviewBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
